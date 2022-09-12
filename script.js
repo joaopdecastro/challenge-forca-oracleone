bc6c2fa53a5908b7884a7c276ece549e59029b14
joaopdecastro <joaopaulocastro.s@gmail.com>
2022-09-12 15:26:04 -0300

    Evolução do HTML e do javascript com criação da lógica de sorteio das palavras inseridas

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function limpaInput() {
-    document.getElementById('inputPalavras').value =''
+//funções de navegação
+
+function telaInicial(){
+    document.getElementById('main-index').style.display = ''
+    document.getElementById('main-addpalavras').style.display = 'none'
+    document.getElementById('main-game').style.display = 'none'
 }
 
-function redirecionaGame () {
-    window.location.replace('jogo.html')
+function iniciaJogo () {
+    document.getElementById('main-index').style.display = 'none'
+    document.getElementById('main-addpalavras').style.display = 'none'
+    document.getElementById('main-game').style.display = ''
+    sorteiaPalavra()
+}
+
+function telaPalavra () {
+    document.getElementById('main-index').style.display = 'none'
+    document.getElementById('main-game').style.display = 'none'
+    document.getElementById('main-addpalavras').style.display = ''
+}
+
+//funções referentes à adição de palavras
+
+function limpaInput() {
+    document.getElementById('inputPalavras').value =''
 }
 
 function adicionaPalavras () {
@@ -17,14 +36,38 @@ function adicionaeSalva () {
     let inputPalavras = document.getElementById('inputPalavras')
     bancoPalavras.push(inputPalavras.value.toUpperCase())
     console.log(bancoPalavras)
-    limpaInput()
-    redirecionaGame()
+    iniciaJogo()
 }
 
-const bancoPalavras = []
+//lógica do jogo
+
+function sorteiaPalavra() {
+    let sorteia = bancoPalavras[Math.floor(Math.random() * bancoPalavras.length)]
+    palavraEscolhida = sorteia
+}  //função adicionada à função de início do jogo
+
+//botões index
+
+let botaoIniciarJogo = document.getElementById('iniciarJogo')
+botaoIniciarJogo.onclick = iniciaJogo
+
+let menuAdicionar = document.getElementById('adicionarPalavra')
+menuAdicionar.onclick = telaPalavra
+
+//botões da tela adicionar palavras
 
 let botaoadicionaoutro = document.getElementById('but-adicionarout')
 botaoadicionaoutro.onclick = adicionaPalavras
 
 let botaoadicionasalva = document.getElementById('but-salvar')
 botaoadicionasalva.onclick = adicionaeSalva
+
+let botaoCancelar = document.getElementById('but-cancelar')
+botaoCancelar.onclick = telaInicial
+
+
+telaInicial()
+
+const bancoPalavras = []
+
+let palavraEscolhida = ''
